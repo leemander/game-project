4fecc5082cadecb388be3fa83676ec0417b640d8
Lee Mander <lee.mander@gmail.com>
2023-11-13 11:49:41 +0000

    Add get endpoints

--- server/server.js
+++ server/server.js
@@ -3,8 +3,25 @@ const cors = require("cors");
 require("dotenv").config();
 const PORT = process.env.PORT || 8080;
 const app = express();
+
 app.use(cors());
+app.use(express.json());
+
+const mongoose = require("mongoose");
+const Game = require("./models/game");
+const Review = require("./models/review");
+mongoose.connect(process.env.API_GAME_KEY);
 
 // add your endpoints here
+
+app.get("/games", async (req, res) => {
+  const games = await Game.find(req.query);
+  res.json(games);
+});
+
+app.get("/reviews", async (req, res) => {
+  const reviews = await Review.find(req.query);
+  res.json(reviews);
+});
 
 app.listen(PORT, () => console.log(`App is running PORT ${PORT}`));
