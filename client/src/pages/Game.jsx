ce9559b56948a0297bf6c5871543720c03587fda
PatrickPatch <patrickclarke1983@gmail.com>
2023-11-16 09:41:12 +0000

    added game page

--- client/src/pages/Game.jsx
+++ client/src/pages/Game.jsx
@@ -1,1 +1,33 @@
-export default function Game() {}
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import Reviews from "../components/Reviews";
+
+export default function Game() {
+  const params = useParams();
+
+  useEffect(() => {
+    getGame();
+  }, []);
+
+  async function getGame() {
+    const API = `https://hapigamr.onrender.com/games?_id=${params.id}`;
+    const res = await axios.get(API);
+    setGame(res.data[0]);
+  }
+
+  const [game, setGame] = useState({});
+
+  return (
+    <main>
+      <h2>{game.title}</h2>
+      <p>{game.genre}</p>
+      <img src={game.boxArtUrl}></img>
+      <h2>{game.releaseYear}</h2>
+      <p>{game.userRating}</p>
+      <p>{game.description}</p>
+      {/* <Reviews /> */}
+      {/* <button onClick={toggleReviewModal}>Add Review</button> */}
+    </main>
+  );
+}
