6bd5c7c8c6068494cf6fad07e07ff80012759e43
RORY MCCONNELL <skunkbentley@hotmail.com>
2023-11-16 11:27:15 +0000

    about page done

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,3 +1,85 @@
 export default function About() {
-  return <h2>Hello</h2>;
+  return (
+    <div>
+      <h1 className="AbouthAPIgamr">About hAPIgamr</h1>
+
+      <section>
+        <p>
+          Your personal online haven for gaming enthusiasts! At hAPIgamr, we've
+          reimagined the gaming experience, creating a space where your gaming
+          journey becomes a shared adventure.
+        </p>
+      </section>
+
+      <section>
+        <h3>Our Unique Platform</h3>
+        <ul>
+          <li>
+            <strong>Your Gaming Library:</strong> hAPIgamr is more than just a
+            platform; it's your personalized gaming library. It's a space where
+            you and your friends can curate and share your gaming experiences.
+            Keep track of every game you've played, rate them, and describe your
+            adventures.
+          </li>
+          <li>
+            <strong>Community-Powered Reviews:</strong> Dive into a wealth of
+            game reviews from the hAPIgamr community. Discover hidden gems,
+            share your insights, and explore a vast collection of reviews that
+            go beyond traditional ratings. Our community-driven approach ensures
+            that you get recommendations from real gamers, just like you.
+          </li>
+          <li>
+            <strong>Connect and Play Together:</strong> Discover games that
+            resonate with you and your friends. Organize gaming sessions, share
+            strategies, and make every gaming moment memorable. hAPIgamr is not
+            just a platform; it's a community where like-minded gamers come
+            together to celebrate their love for gaming.
+          </li>
+        </ul>
+      </section>
+
+      <section>
+        <h3>Features We Love</h3>
+        <ul>
+          <li>
+            <strong>API-Powered Reviews:</strong> Access a vast database of
+            games to review using our API integration.
+          </li>
+          <li>
+            <strong>Auth0 Integration:</strong> Securely log in to hAPIgamr
+            using Auth0. Your gaming history, reviews, and interactions are
+            personal, and we take your privacy seriously. Enjoy a seamless and
+            safe experience every time you log in.
+          </li>
+          <li>
+            <strong>Write Your Story:</strong> Become a part of the hAPIgamr
+            narrative by writing your own game reviews. Share your thoughts,
+            experiences, and recommendations with the community. Your voice
+            matters, and together, we build a comprehensive library of authentic
+            gaming experiences.
+          </li>
+          <li>
+            <strong>User-Controlled Reviews:</strong> Your reviews, your
+            control. Edit, update, or delete your reviews at any time. We
+            believe in giving you the power to curate your gaming journey on
+            hAPIgamr.
+          </li>
+        </ul>
+      </section>
+
+      <section>
+        <h2>Join the hAPIgamr Community</h2>
+        <p>
+          Ready to level up your gaming experience? Join hAPIgamr now and become
+          a part of a community that understands the true essence of gaming.
+          Whether you're a casual player or a dedicated gamer, hAPIgamr is your
+          space to connect, share, and game on.
+        </p>
+      </section>
+
+      <section className="createdBy">
+        <h3>Created by Lee Mander, Patrick Clarke and Rory McConnell</h3>
+      </section>
+    </div>
+  );
 }
